06669b3be3b40c343931db61b3855cfa7461594b
Elizavetasulima <elizvood@gmail.com>
2023-09-14 02:38:57 +0300

    cypress

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -1,4 +1,4 @@
 
-let number1 = Math.floor(Math.random() * 100)
-let result = number1 % 2 === 0 ? "четное" : "нечетное"
-console.log(number1 + ": Это " + result + " число")
+let number1 = Math.floor(Math.random() * 100);
+let result = number1 % 2 === 0 ? "четное" : "нечетное";
+console.log(number1 + ": Это " + result + " число");
