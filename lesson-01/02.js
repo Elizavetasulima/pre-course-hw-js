a97b6d1b34c95e74a5ded06866890330f34ce18d
Elizavetasulima <elizvood@gmail.com>
2023-09-14 20:35:30 +0300

    «»1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,3 @@
 let number1 = Math.floor(Math.random() * 100);
 let result = number1 % 2 === 0 ? "четное" : "нечетное";
-console.log(`[${number1}]: Это ${result} число`);
+console.log(`«[${number1}]: Это ${result} число»`);
