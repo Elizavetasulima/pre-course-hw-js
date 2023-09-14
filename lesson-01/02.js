fa02f428608c079050804ce55a58afac24f3d0e5
Elizavetasulima <elizvood@gmail.com>
2023-09-14 13:23:31 +0300

    even or odd

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,7 @@
 let number1 = Math.floor(Math.random() * 100);
 let result = number1 % 2 === 0 ? "четное" : "нечетное";
-console.log(number1 + ": Это " + result + " число");
+//console.log(number1 + ": Это " + result + " число");
+//console.log(result(4));  // Вывод: четное
+//console.log(result(7));  // Вывод: нечетное
+//console.log(result(0));
+console.log(`${number1}: Это ${result} число`);
