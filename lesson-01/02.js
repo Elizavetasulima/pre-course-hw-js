76f262babc26479215b881a9e0a37592efa2d2fb
Elizavetasulima <elizvood@gmail.com>
2023-09-15 04:20:50 +0300

    start3.3

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,17 @@
+let number1 = Math.floor(Math.random() * 100);
 
+if (number1 % 2===0) {
+    console.log (`«[${number1}]: Это четное число»`);
+} else {
+    console.log(`«[${number1}]: Это нечетное число»`);
+}
 
 let number = 1;
-console.log(number++); 
-console.log(++number); 
-console.log(number--); 
-console.log(--number); 
+
+//--number   number--  ++number   number++
+
+--number; console.log(number + 1); 
+number--; console.log(number + 4); 
+++number; console.log(number + 3); 
+number++; console.log(number); 
+
