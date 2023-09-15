76523134e3b3a88040629931c6dab7c3fd4736f4
Elizavetasulima <elizvood@gmail.com>
2023-09-15 03:06:22 +0300

    start3

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -6,6 +6,13 @@ if (number1 % 2===0) {
     console.log(`«[${number1}]: Это нечетное число»`);
 }
 
+let number =1;
+
+console.log(number++); 
+console.log(++number); 
+console.log(number++); 
+console.log(--number - --number); 
+
 
 
 
