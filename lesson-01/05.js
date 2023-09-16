aeb30bd5d62a7d9ff361ecba72b201d8f5aba1a5
Elizavetasulima <elizvood@gmail.com>
2023-09-17 00:43:48 +0300

    050505qwerty

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -4,7 +4,7 @@ switch (true) {
     case rule.length < 25:
         console.log ("Все таки нет правил без исключения");
         break;
-        case rule.length < 25:
+        case rule.length > 25:
         console.log ("У меня 100 пудов все получится");
         break;
         default:
