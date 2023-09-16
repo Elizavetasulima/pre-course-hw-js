959b95f866ab9c3be26a45c14740e0a3670a72ca
Elizavetasulima <elizvood@gmail.com>
2023-09-16 23:22:01 +0300

    04/03

--- lesson-01/04.js
+++ lesson-01/04.js
@@ -1,9 +1,9 @@
 let x = 5;
 x += 3;
-console.log(x(8));
+console.log(x);
 x *= 2;
-console.log(x(16));
+console.log(x);
 x -= 4;
-console.log(x(12));
+console.log(x);
 x /= 3;
-console.log(x(4));
+console.log(x);
