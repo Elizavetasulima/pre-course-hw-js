4a9893c79cf5e5aa8a543fca157daa163a6bc770
Elizavetasulima <elizvood@gmail.com>
2023-12-17 22:50:23 +0300

    040504izm

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,7 +2,15 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-console.log(passport) 
-let passport1=passport; 
-passport1.name='Ivan'
+
+let passport1 = {}; // новый пустой объект
+
+// давайте скопируем все свойства user в него
+for (let key in passport) {
+passport1[key] = passport[key];
+}
+// теперь clone это полностью независимый объект с тем же содержимым
+passport1.name = "Ivan"; // изменим в нём данные
+
+console.log(passport)
 console.log(passport1);
