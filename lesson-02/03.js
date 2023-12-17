057f8de140b7f907286ff443583e749a1dd5361c
Elizavetasulima <elizvood@gmail.com>
2023-12-17 22:00:05 +0300

    030303izm

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,8 +1,8 @@
 const giveMeRightNumbers = (value1, value2, maximum) => {
     if (value1 === value2 || value1 <= maximum) {
-        console.log('Something wrong!');
-    } else {
         console.log('Success!');
+    } else {
+        console.log('Something wrong!');
     }
 }
 giveMeRightNumbers(203, 202, 201)
