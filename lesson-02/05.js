1c9e15f0994553b3486929c9bff1f110d7d7c60e
Elizavetasulima <elizvood@gmail.com>
2023-12-17 22:35:59 +0300

    0505izm

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,12 +2,7 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-
-let passport1 = {
-    name: "Ivan",
-    surname: "Petrov",
-};
-
-
-console.log(passport);
+console.log(passport) 
+let passport1=passport; 
+passport1.name='Ivan'
 console.log(passport1);
