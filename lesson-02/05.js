b9d654583dd4ed28a47120ff440122a1c5e0528e
Elizavetasulima <elizvood@gmail.com>
2023-12-18 08:48:21 +0300

    clone

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,23 +2,20 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-let passportCopy = passport;
-passport = {
-    name: "Ivan",
-    surname: "Petrov",
-};
+
 
 // let passportCopy = Object.assign ({}, passport)
 // passportCopy.name = 'Ivan'
 
 
-// let passport1 = {}; // новый пустой объект
+let clonePassport = {}; // новый пустой объект
 
-// // давайте скопируем все свойства user в него
-// for (let key in passport) {
-// passport1[key] = passport[key];
-// }
-// // теперь clone это полностью независимый объект с тем же содержимым
-// passport1.name = "Ivan"; // изменим в нём данные
-console.log(passportCopy);
+// давайте скопируем все свойства user в него
+for (let key in passport) {
+    clonePassport[key] = passport[key];
+}
+// теперь clone это полностью независимый объект с тем же содержимым
+clonePassport.name = "Ivan"; // изменим в нём данные
 console.log(passport)
+console.log(clonePassport);
+
